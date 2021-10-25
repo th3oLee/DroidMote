ed585efc5927ea3ecdd19831e7dfbc3048737771
Théo <conoracre62@gmail.com>
2021-10-25 19:32:51 +0200

    Add button script

--- Assets/Scripts/RemoteDroidController.cs
+++ Assets/Scripts/RemoteDroidController.cs
@@ -7,12 +7,20 @@ public class RemoteDroidController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        foreach (var device in Microphone.devices)
+    {
+        Debug.Log("Name: " + device);
+    }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void pressButton(string buttonName)
+    {
+        Debug.Log(buttonName);
     }
 }
