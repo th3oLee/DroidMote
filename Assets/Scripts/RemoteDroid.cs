9f8daa640714c478adbf40e8fb4d9781ce227935
th3oLee <conoracre62@gmail.com>
2021-10-26 15:53:09 +0200

    Add client server

---
+++ Assets/Scripts/RemoteDroid.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemoteDroid : MonoBehaviour
+{
+    TCPTestClient tcp;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tcp = new TCPTestClient();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 acc = Input.acceleration;
+        //tcp.SendMessage("(" + Input.acceleration.x.ToString() + ";" + Input.acceleration.y.ToString() + ")");
+    }
+
+    public void clickOnButton(string buttonName)
+    {
+        tcp.SendMessage(buttonName);
+    }
+
+
+}
